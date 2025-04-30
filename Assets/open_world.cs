697267686ac0848a5a65eff536cf91f02e795e30
nityashah1811 <nityashah1811@gmail.com>
2025-04-29 21:07:44 -0700

    ADDED START SCREEN AFTER REVERT EVERYTHING WORKS

--- Assets/open_world.cs
+++ Assets/open_world.cs
@@ -1,23 +1,14 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-public class open_world1 : MonoBehaviour
+public class CanvasRemover : MonoBehaviour
 {
-    // Assign these in the Unity Inspector
-    public Canvas currentCanvas; // The canvas currently displayed
-    public Canvas targetCanvas;  // The canvas to open
+    public GameObject canvasToDisable;
 
-    // This function will be called when the button is pressed
     public void SwitchCanvas()
     {
-        if (currentCanvas != null)
+        if (canvasToDisable != null)
         {
-            currentCanvas.gameObject.SetActive(false); // Deactivate the current canvas
-        }
-
-        if (targetCanvas != null)
-        {
-            targetCanvas.gameObject.SetActive(true);  // Activate the target canvas
+            canvasToDisable.SetActive(false);
         }
     }
 }
